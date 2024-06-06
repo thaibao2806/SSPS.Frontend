ca474eaaf73dc172755adfeee783c43765fb68d9
Admin <bthai865@gmail.com>
2024-06-06 11:19:25 +0700

    fix: remove attribute config

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  "@fullcalendar/react/dist/vdom",],
+  plugins: [react(),],
   optimizeDeps: {
     exclude: ['js-big-decimal']
   }
